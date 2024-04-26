f2bc4a29d050382867ac23ee0bf55b866d76172c
Ritesh Dhakad <134867726+riteshdhakad04@users.noreply.github.com>
2024-04-26 13:39:38 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,9 +1,30 @@
 function smallestMissingPositiveInteger(nums) {
-
-
-    // Implement the function smallest_missing_positive_integer
+  const n = nums.length;
   
+   let j = 0;
+  for (let i = 0; i < n; i++) {
+      if (nums[i] <= 0) {
+          [nums[i], nums[j]] = [nums[j], nums[i]];
+          j++;
+      }
   }
   
-  module.exports = smallestMissingPositiveInteger;
+   const x = nums.slice(j);
+  
+   for (let i = 0; i < x.length; i++) {
+      const index = Math.abs(x[i]) - 1;
+      if (index < x.length && x[index] > 0) {
+          x[index] = -x[index];
+      }
+  }
+  
+   for (let i = 0; i < x.length; i++) {
+      if (x[i] > 0) {
+          return i + 1;
+      }
+  }
+  
+   return x.length + 1;
+}
 
+module.exports = smallestMissingPositiveInteger;
